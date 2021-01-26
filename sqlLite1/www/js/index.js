c3d52bc3cb4859533df9d141505f747130791bc6
chavangorakh1999 <chavangorakh19999@gmail.com>
2021-01-26 10:57:14 +0530

    creating workshop project

--- sqlLite1/www/js/index.js
+++ sqlLite1/www/js/index.js
@@ -1,29 +1,198 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+var createStatement =
+  "CREATE TABLE IF NOT EXISTS Contacts (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, useremail TEXT)";
 
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
+var selectAllStatement = "SELECT * FROM Contacts";
 
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
+var insertStatement = "INSERT INTO Contacts (username, useremail) VALUES (?,?)";
 
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+var updateStatement =
+  "UPDATE Contacts SET username = ?, useremail = ? WHERE id=?";
+
+var deleteStatement = "DELETE FROM Contacts WHERE id=?";
+
+var dropStatement = "DROP TABLE Contacts";
+
+var db = openDatabase("AddressBook", "1.0", "Address Book", 200000); // Open SQLite Database
+
+var dataset;
+
+var DataType;
+
+function initDatabase() {
+  // Function Call When Page is ready.
+
+  try {
+    if (!window.openDatabase) {
+      // Check browser is supported SQLite or not.
+
+      alert("Databases are not supported in this browser.");
+    } else {
+      createTable(); // If supported then call Function for create table in SQLite
+    }
+  } catch (e) {
+    if (e == 2) {
+      // Version number mismatch.
+
+      console.log("Invalid database version.");
+    } else {
+      console.log("Unknown error " + e + ".");
+    }
+
+    return;
+  }
 }
+
+function createTable() {
+  // Function for Create Table in SQLite.
+
+  db.transaction(function (tx) {
+    tx.executeSql(createStatement, [], showRecords, onError);
+  });
+}
+
+function insertRecord() {
+  // Get value from Input and insert record . Function Call when Save/Submit Button Click..
+
+  var usernametemp = $("input:text[id=username]").val();
+
+  var useremailtemp = $("input:text[id=useremail]").val();
+  db.transaction(function (tx) {
+    tx.executeSql(
+      insertStatement,
+      [usernametemp, useremailtemp],
+      loadAndReset,
+      onError
+    );
+  });
+
+  //tx.executeSql(SQL Query Statement,[ Parameters ] , Sucess Result Handler Function, Error Result Handler Function );
+}
+
+function deleteRecord(id) {
+  // Get id of record . Function Call when Delete Button Click..
+
+  var iddelete = id.toString();
+
+  db.transaction(function (tx) {
+    tx.executeSql(deleteStatement, [id], showRecords, onError);
+    alert("Delete Sucessfully");
+  });
+
+  resetForm();
+}
+
+function updateRecord() {
+  // Get id of record . Function Call when Delete Button Click..
+
+  var usernameupdate = $("input:text[id=username]").val().toString();
+
+  var useremailupdate = $("input:text[id=useremail]").val().toString();
+
+  var useridupdate = $("#id").val();
+
+  db.transaction(function (tx) {
+    tx.executeSql(
+      updateStatement,
+      [usernameupdate, useremailupdate, Number(useridupdate)],
+      loadAndReset,
+      onError
+    );
+  });
+}
+
+function dropTable() {
+  // Function Call when Drop Button Click.. Talbe will be dropped from database.
+
+  db.transaction(function (tx) {
+    tx.executeSql(dropStatement, [], showRecords, onError);
+  });
+
+  resetForm();
+
+  initDatabase();
+}
+
+function loadRecord(i) {
+  // Function for display records which are retrived from database.
+
+  var item = dataset.item(i);
+
+  $("#username").val(item["username"].toString());
+
+  $("#useremail").val(item["useremail"].toString());
+
+  $("#id").val(item["id"].toString());
+}
+
+function resetForm() {
+  // Function for reset form input values.
+
+  $("#username").val("");
+
+  $("#useremail").val("");
+
+  $("#id").val("");
+}
+
+function loadAndReset() {
+  //Function for Load and Reset...
+
+  resetForm();
+
+  showRecords();
+}
+
+function onError(tx, error) {
+  // Function for Hendeling Error...
+
+  alert(error.message);
+}
+
+function showRecords() {
+  // Function For Retrive data from Database Display records as list
+
+  //I WANT THIS TO CLEAR TO DIPLAY ALL RECORDS INTO IT
+  $("#results").html("");
+
+  db.transaction(function (tx) {
+    tx.executeSql(selectAllStatement, [], function (tx, result) {
+      dataset = result.rows;
+
+      for (var i = 0, item = null; i < dataset.length; i++) {
+        item = dataset.item(i);
+
+        var v1 =
+          "<li>" +
+          item["username"] +
+          " , " +
+          item["useremail"] +
+          "    " +
+          '<a href="#" onclick="loadRecord(' +
+          i +
+          ');">edit</a>' +
+          "    " +
+          '<a href="#" onclick="deleteRecord(' +
+          item["id"] +
+          ');">delete</a></li>';
+
+        $("#results").append(v1);
+      }
+    });
+  });
+}
+
+$(document).ready(function () // Call function when page is ready for load..
+
+{
+  $("body").fadeIn(3000); // Fede In Effect when Page Load..
+
+  initDatabase();
+
+  $("#submitButton").click(insertRecord); // Register Event Listener when button click.
+
+  $("#btnUpdate").click(updateRecord);
+
+  $("#btnReset").click(resetForm);
+
+  $("#btnDrop").click(dropTable);
+});
